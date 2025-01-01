11c8f36438b5ef4df54aec4d7e9133cf16ca6a06
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-01 23:59:22 +0530

    Conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Khalid::Hi Veeru"
+echo "Veeru::Hi Khalid"
+echo "Khalid::How are you"
+echo "Veeru::iam fine how about you"
---
+++ 03-Variables latest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Person1 ="Khalid"
+Person2 = "Veeru"
+echo "$Person1::Hi $Person2"
+echo "$Person2::Hi $Person1"
+echo "$Person1::How are you"
+echo "$Person2::iam fine how about you"
