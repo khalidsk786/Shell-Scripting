11cd74c20d6f35f76b6866c58da4360feb71cc7d
khalidsk786 <khalidaws2024.com>
2025-01-02 22:20:37 +0530

    converation between Khalid and Veeru updated WITH ARGUMENTS thanks...........

--- 04-Variables.sh
+++ 04-Variables.sh
@@ -4,4 +4,4 @@ PERSON2=$2
 echo "$PERSON1::Hi $PERSON2"
 echo "$PERSON2::Hi $PERSON1"
 echo "$PERSON1::How are you.........?"
-echo "$PERSON2::iam fine how about you.........."
+echo "$PERSON2::iam fine how about you,thnaks for asking me.........."
