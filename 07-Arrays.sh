9887d2cc61a8bb81c6ce0d6531c465cbc2a0d94e
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-04 20:12:52 +0530

    Sum of two numers

--- 07-Arrays.sh
+++ 07-Arrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MOVIES=("rrr" "devara" "bahubali")
 
-echo "First movie: ${MOVIES[0]}"
-echo "second movie: ${MOVIES[1]}"
-echo "3rd movie: ${MOVIES[2]}"
-echo "All Movies: ${MOVIES[@]}"
+echo "First movie:${MOVIES[0]}"
+echo "second movie:${MOVIES[1]}"
+echo "3rd movie:${MOVIES[2]}"
+echo "All Movies:${MOVIES[@]}"
