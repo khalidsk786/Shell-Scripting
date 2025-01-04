d30969ed0820020825d576202c59822dcdbc1e9a
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-04 19:02:46 +0530

    Sum of two numers

--- 06-Data-type.sh
+++ 06-Data-type.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 NUMBER1=$1
-NUMBER2=$1
+NUMBER2=$2
 TIMESTAMP=$(date)
-echo "script executed:$date"
-SUM=$(($NUMBER1+$NUMBER2))
+echo "script executed:$date"    
+SUM=$($NUMBER1+$NUMBER2)
 echo "sum of $NUMBER1 AND $NUMBER2:$SUM"
