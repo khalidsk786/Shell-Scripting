624e27b9897ab1540baab327ba35a2fad9a1ee20
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-04 22:06:40 +0530

    Sum of two numers

---
+++ 08-Special-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "All variables passed:$@"
+echo "number of varaibles:@#"
+echo "to know vscode script name:$0"
+echo "present working directory:$PWD"
+echo "home directory of current user:$HOME"
+echo "which user is running in script:$USER"
+echo "processs id of current user:$$"
+echo "sleep 60"
+echo "process id of last command in background:$#"
