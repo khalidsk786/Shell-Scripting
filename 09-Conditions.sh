e0eb80f674c46f4d44987d879f57334dc0f0c374
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-04 23:23:26 +0530

    Sum of two numers

--- 09-Conditions.sh
+++ 09-Conditions.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 if [$NUMBER -ge 100]
 then
-echo "Given number is greaterthen 100"
+echo "Given number is greaterthen or equal to 100"
 else
-echo "Given number is lessthan 100"
+echo "Given number is lessthan or equal to 100"
 fi
