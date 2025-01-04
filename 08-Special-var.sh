ca6e6143af1642a95812126bc9b923b13e93ff62
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-04 22:08:38 +0530

    Sum of two numers

--- 08-Special-var.sh
+++ 08-Special-var.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "All variables passed:$@"
-echo "number of varaibles:@#"
+echo "number of varaibles:$#"
 echo "to know vscode script name:$0"
 echo "present working directory:$PWD"
 echo "home directory of current user:$HOME"
