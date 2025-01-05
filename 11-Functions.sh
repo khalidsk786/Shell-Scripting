7e6492ef0e8c78f1fda1ea13ab5ec79253f6646a
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-05 17:13:31 +0530

    Validating functions

--- 11-Functions.sh
+++ 11-Functions.sh
@@ -4,38 +4,38 @@ VALIDATE (){
 if [$1 -ne 0]
 then
 
- echo"$2...FAILURE"
- exit 1
+    echo"$2...FAILURE"
+    exit 1
 
 else
-echo "$2...SUCCESS"
+     echo "$2...SUCCESS"
 fi
 }
 
 
-if[ $? -ne 0 ]
+if [ $? -ne 0 ]
 
  then
-  dnf install mysql -y
-  VALIDATE $?
+    dnf install mysql -y
+    VALIDATE $?
 exit 1
 
 else
-  echo "MY SQL installed already"
+    echo "MY SQL installed already"
 fi 
-
+    
 
 
 
  
-if[ $? -ne 0 ]
+if [ $? -ne 0 ]
 
 then
- dnf install git -y
- VALIDATE $?
- exit 1
+    dnf install git -y
+    VALIDATE $?
+    exit 1
 
 else
-  echo "MY git installed already"
+    echo "MY git installed already"
 
  fi
