5ac6d259dc8bb8b0cdbaeb4cd9e3f809fbec3bf4
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-05 20:21:40 +0530

    Validating functions

---
+++ 12-colors.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+R="\e[31m]"
+G="\e[32m]"
+Y="\e[33m]"
+
+VALIDATE (){
+if [$1 -ne 0]
+then
+
+    echo -e "$2...$R FAILURE"
+    exit 1
+
+else
+     echo -e "$2... $G SUCCESS"
+fi
+}
+
+
+if [ $? -ne 0 ]
+
+ then
+    dnf install mysql -y
+    VALIDATE $?
+exit 1
+
+else
+    echo -e "MY SQL  $Y installed already"
+fi 
+    
+
+
+
+ 
+if [ $? -ne 0 ]
+
+then
+    dnf install git -y
+    VALIDATE $?
+    exit 1
+
+else
+    echo -e "MY git -$Y installed already"
+
+ fi
