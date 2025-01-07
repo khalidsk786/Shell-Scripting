95c8e82395746bcc63a1d2280ed0e67a33dc656d
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-07 21:46:00 +0530

    Excuting install script code

--- 13.logs.sh
+++ 13.logs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R="\e[31m]"
-G="\e[32m]"
-Y="\e[33m]"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 LOGS_FOLDER="var/log/shellscripting.logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
