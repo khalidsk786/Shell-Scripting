96a79d36ae8206b37b23ce41a3cb149a95f83dbc
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-07 22:13:52 +0530

    Excuting install script code

--- 13.logs.sh
+++ 13.logs.sh
@@ -12,43 +12,33 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME= "$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
-
 if [$1 -ne 0]
 then
-  echo "$2... $R Failure $N"
+  echo -e "$2... $R Failure $N"
   exit 1
 else
-   echo "$2... $G success $N"
-        
+   echo -e "$2... $G success $N"
 fi  
 }
-
 echo "script excuted at: $TIMESTAMP" &>>$LOG_FILE_NAME
 if [ $USERID -ne 0 ]
 then
    echo "Execute with sudo access for installation"
    exit 1 #other than 0 
 fi
-
 dnf list installed mysql &>>$LOG_FILE_NAME
-
 if [ $? -ne 0 ]
 then
-
- dnf install mysql -y &>>$LOG_FILE_NAME
-
- VALIDATE $? "installing mysql" 
+  dnf install mysql -y &>>$LOG_FILE_NAME
+  VALIDATE $? "installing mysql" 
 else
- echo "my sql installed already... $Y installed $N"
+  echo -e "my sql installed already... $Y installed $N"
 fi
-
 dnf list installed git &>>$LOG_FILE_NAME
-
 if [ $? -ne 0]
 then
  dnf install git -y &>>$LOG_FILE_NAME
-       
  VALIDATE $? "installing git"
 else
- echo "git installed already.... $Y installed"
+ echo -e "git installed already.... $Y installed"
 fi
