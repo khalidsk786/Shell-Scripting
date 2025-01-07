564f52bcb312e6b73b8fdb7d1652bf9bef1c5e32
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-07 15:56:38 +0530

    Excuting install script code

--- 10-Install-script.sh
+++ 10-Install-script.sh
@@ -3,9 +3,9 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-   echo "Execute with sudo access"
+   echo "Execute with sudo access for installation"
 exit 1 #other than 0 
 fi
-dnf install mysql -y
-dnf install git -y
+dnf install mysqll -y
+dnf install gitt -y
 
