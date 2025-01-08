1f08429c2480b3edfe6753cc36b361a7180ffa20
Shaik <Khalid.Shaik@lionbridge.com>
2025-01-08 12:39:32 +0530

    Excuting install script code

--- 16-Loopsconcept.sh
+++ 16-Loopsconcept.sh
@@ -8,8 +8,8 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+#TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
